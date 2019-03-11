60519d5a2d2689acf54d968fbee7528b9264cfe8
axytho <jojojo.jonas888@hotmail.com>
2019-03-11 16:24:06 +0100

    super basic put/post

--- src/htmlclient/PutCommand.java
+++ src/htmlclient/PutCommand.java
@@ -1,23 +1,23 @@
 package htmlclient;
 
 /**
- * A class for the HTML POST command implementation
+ * A class for the HTML Put command implementation
  *
  * 
  */
 
-public class PostCommand extends Command {
+public class PutCommand extends Command {
 
 	/**
-	 * Constructs a new PostCommand.
+	 * Constructs a new PutCommand.
 	 *
-	 * @param host 		The host for this PostCommand
-	 * @param path  	The path of this PostCommand
-	 * @param writer 	The writer for this PostCommand
-	 * @param reader 	The reader for this PostCommand
+	 * @param host 		The host for this PutCommand
+	 * @param path  	The path of this PutCommand
+	 * @param writer 	The writer for this PutCommand
+	 * @param reader 	The reader for this PutCommand
 	 * @effect 			A new Command is constructed
 	 */
-	public PostCommand(String host, String path, OutputStream writer, InputStream reader) 
+	public PutCommand(String host, String path, OutputStream writer, InputStream reader) 
 			throws IllegalArgumentException {
 		super(host, path, writer, reader);
 	}
